7889a06e23593f6168ba1fcf7fb009d00c17f73b
ac-scruffyk <acarva1@yahoo.com>
2024-05-05 19:38:21 -0500

    viewport and frame timing updates

--- script.js
+++ script.js
@@ -160,132 +160,142 @@ document.addEventListener('click', function(e){
   shootingStarsArray.push(new ShootingStar(e.clientX, e.clientY, Math.ceil(Math.random() * 4)));
 });
 
+
+let currentTime = Date.now();
+
 function draw() {
-    canDraw.clearRect(0,0,maxX,maxY);
-    //Draw the backgrounds
-    let bgGrad = canDraw.createRadialGradient(moonX, moonY, 0, moonX, moonY, Math.max(innerWidth, innerHeight));
-    bgGrad.addColorStop(0, 'rgb(60,60,120)');
-    bgGrad.addColorStop(1, 'rgb(5,5,15)');
-    canDraw.fillStyle = bgGrad;
-    canDraw.fillRect(0,0,maxX,maxY);
-  	bgTop = moonY / maxY;
-    
-    //Draw starfield
-    for (let i = 0; i < starArray.length; i++) {
-        canDraw.globalAlpha = 1 - (Math.random()/1.5);
-        canDraw.fillStyle = "#FAF";
-        canDraw.fillRect(starArray[i].x,starArray[i].y,1,1);
+  let frameTime = Date.now();
+  if (frameTime - currentTime < 16) {
+    window.requestAnimationFrame(draw);
+    return;
+  }
+  currentTime = frameTime;
+
+  canDraw.clearRect(0,0,maxX,maxY);
+  //Draw the backgrounds
+  let bgGrad = canDraw.createRadialGradient(moonX, moonY, 0, moonX, moonY, Math.max(innerWidth, innerHeight));
+  bgGrad.addColorStop(0, 'rgb(60,60,120)');
+  bgGrad.addColorStop(1, 'rgb(5,5,15)');
+  canDraw.fillStyle = bgGrad;
+  canDraw.fillRect(0,0,maxX,maxY);
+  bgTop = moonY / maxY;
+  
+  //Draw starfield
+  for (let i = 0; i < starArray.length; i++) {
+      canDraw.globalAlpha = 1 - (Math.random()/1.5);
+      canDraw.fillStyle = "#FAF";
+      canDraw.fillRect(starArray[i].x,starArray[i].y,1,1);
+  }
+  canDraw.globalAlpha = 1;
+  
+  //Shooting star drawing and logic
+  let starRand = Math.random();
+  if (starRand <= 0.0001) {
+    shootingStarsArray.push(new ShootingStar(Math.ceil(Math.random() * maxX), -10, Math.ceil(Math.random() * 4)));
+  } else if (starRand <= 0.001) {
+    shootingStarsArray.push(new ShootingStar(Math.ceil(Math.random() * maxX), -10));
+  }
+
+  canDraw.strokeStyle = "#FFF";
+  shootingStarsArray.forEach(star => {
+    let index = shootingStarsArray.indexOf(star);
+
+    // if stars are particularly large/slow, draw a halo around them, with less alpha as they get closer to the center of the screen
+    if (star.size >= 3) {
+      let starGrad = canDraw.createRadialGradient(star.x, star.y, 0, star.x, star.y, 5 * star.size);
+      starGrad.addColorStop(0, 'rgba(255,255,255,' + 0.1 * star.size + ')');
+      starGrad.addColorStop(1, 'rgba(255,255,255,0)');
+      canDraw.fillStyle = starGrad;
+      canDraw.fillRect(star.x - 10 * star.size / 2, star.y - 10 * star.size / 2, 30 * star.size, 30 * star.size);
     }
-    canDraw.globalAlpha = 1;
-    
-    //Shooting star drawing and logic
-    let starRand = Math.random();
-    if (starRand <= 0.0001) {
-      shootingStarsArray.push(new ShootingStar(Math.ceil(Math.random() * maxX), -10, Math.ceil(Math.random() * 4)));
-    } else if (starRand <= 0.001) {
-      shootingStarsArray.push(new ShootingStar(Math.ceil(Math.random() * maxX), -10));
-    }
 
-    canDraw.strokeStyle = "#FFF";
-    shootingStarsArray.forEach(star => {
-      let index = shootingStarsArray.indexOf(star);
-
-      // if stars are particularly large/slow, draw a halo around them, with less alpha as they get closer to the center of the screen
-      if (star.size >= 3) {
-        let starGrad = canDraw.createRadialGradient(star.x, star.y, 0, star.x, star.y, 5 * star.size);
-        starGrad.addColorStop(0, 'rgba(255,255,255,' + 0.1 * star.size + ')');
-        starGrad.addColorStop(1, 'rgba(255,255,255,0)');
-        canDraw.fillStyle = starGrad;
-        canDraw.fillRect(star.x - 10 * star.size / 2, star.y - 10 * star.size / 2, 30 * star.size, 30 * star.size);
-      }
-
-      canDraw.lineWidth = star.size;
-      canDraw.beginPath();
-      canDraw.moveTo(star.x - star.speed, star.y - star.speed)
-      canDraw.lineTo(star.x, star.y)
-      canDraw.stroke();
-
-      if (star.x > maxX + 50 || star.x < -20 || star.y > maxY + 50) {
-          shootingStarsArray.splice(index, 1);
-      }
-      else {
-          star.x += star.speed;
-          star.y += star.speed;
-      }
-    });
-    
-    //Draw moon and move it
-    let moonGrad = canDraw.createRadialGradient(moonX, moonY, 0, moonX, moonY,40);
-    moonGrad.addColorStop(0.28, 'rgba(255,225,255,1)');
-    moonGrad.addColorStop(0.88, 'rgba(248,200,255,1)');
-    moonGrad.addColorStop(1, 'rgba(248,200,255,0)');
-    canDraw.fillStyle = moonGrad;
+    canDraw.lineWidth = star.size;
     canDraw.beginPath();
-    canDraw.arc(moonX,moonY,40,0,Math.PI*2,0)
-    canDraw.fill();
-    moonX += moonDir;
-    if (moonY > maxY + 50 || moonX > maxX + 50 || moonX < -42) {
-        moonY = moonY;
+    canDraw.moveTo(star.x - star.speed, star.y - star.speed)
+    canDraw.lineTo(star.x, star.y)
+    canDraw.stroke();
+
+    if (star.x > maxX + 50 || star.x < -20 || star.y > maxY + 50) {
+        shootingStarsArray.splice(index, 1);
     }
     else {
-        moonY += 0.05;
+        star.x += star.speed;
+        star.y += star.speed;
     }
-    
-    //Draw buildings - dynamic foreground
-    for (let k = 0; k < buildingArray.length; k++) {
-      canDraw.fillStyle = "#000";
-      canDraw.fillRect(buildingArray[k].x,buildingArray[k].y,buildingArray[k].w,buildingArray[k].h);
-      if (buildingArray[k].triTop === 1) {
-          canDraw.beginPath();
-          canDraw.moveTo(buildingArray[k].x,buildingArray[k].y);
-          canDraw.lineTo(buildingArray[k].x + buildingArray[k].w / 2, buildingArray[k].y - buildingArray[k].winHeight * 4);
-          canDraw.lineTo(buildingArray[k].x + buildingArray[k].w, buildingArray[k].y);
-          canDraw.fill();
-      }
-    }
-    
-    //Draw windows - dynamic foreground
-    for (let j = 0; j < windowLightArray.length; j++) {
-        if (windowLightArray[j].lit) {
-            canDraw.fillStyle = "#EE9";
-            canDraw.fillRect(windowLightArray[j].x,windowLightArray[j].y,6,windowLightArray[j].tall - 4);
-            if (Math.random() < 0.00002) {
-                windowLightArray[j].lit = 0;
-            }
-        }
-        else if (windowLightArray[j].lit === 0) {
-            canDraw.fillStyle = "#020202";
-            canDraw.fillRect(windowLightArray[j].x,windowLightArray[j].y,6,windowLightArray[j].tall - 4);
-            if (Math.random() < 0.00001) {
-                windowLightArray[j].lit = 1;
-            } 
-        }
-    }
-    //Draw the red dot on towers with a width of 10
-    for (let m = 0; m < dotArray.length; m++) {
-        if (dotArray[m].myCounter % 100 > 50) {
-            canDraw.fillStyle = "#F00";
-        }
-        else {
-            canDraw.fillStyle = "#010203";
-        }
+  });
+  
+  //Draw moon and move it
+  let moonGrad = canDraw.createRadialGradient(moonX, moonY, 0, moonX, moonY,40);
+  moonGrad.addColorStop(0.28, 'rgba(255,225,255,1)');
+  moonGrad.addColorStop(0.88, 'rgba(248,200,255,1)');
+  moonGrad.addColorStop(1, 'rgba(248,200,255,0)');
+  canDraw.fillStyle = moonGrad;
+  canDraw.beginPath();
+  canDraw.arc(moonX,moonY,40,0,Math.PI*2,0)
+  canDraw.fill();
+  moonX += moonDir;
+  if (moonY > maxY + 50 || moonX > maxX + 50 || moonX < -42) {
+      moonY = moonY;
+  }
+  else {
+      moonY += 0.05;
+  }
+  
+  //Draw buildings - dynamic foreground
+  for (let k = 0; k < buildingArray.length; k++) {
+    canDraw.fillStyle = "#000";
+    canDraw.fillRect(buildingArray[k].x,buildingArray[k].y,buildingArray[k].w,buildingArray[k].h);
+    if (buildingArray[k].triTop === 1) {
         canDraw.beginPath();
-        canDraw.arc(dotArray[m].x + 0.5,dotArray[m].y,2,0,Math.PI*2);
+        canDraw.moveTo(buildingArray[k].x,buildingArray[k].y);
+        canDraw.lineTo(buildingArray[k].x + buildingArray[k].w / 2, buildingArray[k].y - buildingArray[k].winHeight * 4);
+        canDraw.lineTo(buildingArray[k].x + buildingArray[k].w, buildingArray[k].y);
         canDraw.fill();
-        dotArray[m].myCounter++;
     }
-    canDraw.globalAlpha = 1;
-  	if (moonY > maxY || moonX > maxX + 40 || moonX < -40 && fadeOut < 1) {
-      canDraw.fillStyle = 'rgba(0,0,0,' + fadeOut + ')';
-      canDraw.fillRect(0,0,maxX,maxY);
-      fadeOut += 0.001;
-    }
-  	if (fadeOut >= 1) {
-      fadeOut = 0;
-      moonY = -50;
-      moonX = 100 + (Math.random() * (innerWidth - 100));
-    }
-  	window.requestAnimationFrame(draw);
+  }
+  
+  //Draw windows - dynamic foreground
+  for (let j = 0; j < windowLightArray.length; j++) {
+      if (windowLightArray[j].lit) {
+          canDraw.fillStyle = "#EE9";
+          canDraw.fillRect(windowLightArray[j].x,windowLightArray[j].y,6,windowLightArray[j].tall - 4);
+          if (Math.random() < 0.00002) {
+              windowLightArray[j].lit = 0;
+          }
+      }
+      else if (windowLightArray[j].lit === 0) {
+          canDraw.fillStyle = "#020202";
+          canDraw.fillRect(windowLightArray[j].x,windowLightArray[j].y,6,windowLightArray[j].tall - 4);
+          if (Math.random() < 0.00001) {
+              windowLightArray[j].lit = 1;
+          } 
+      }
+  }
+  //Draw the red dot on towers with a width of 10
+  for (let m = 0; m < dotArray.length; m++) {
+      if (dotArray[m].myCounter % 100 > 50) {
+          canDraw.fillStyle = "#F00";
+      }
+      else {
+          canDraw.fillStyle = "#010203";
+      }
+      canDraw.beginPath();
+      canDraw.arc(dotArray[m].x + 0.5,dotArray[m].y,2,0,Math.PI*2);
+      canDraw.fill();
+      dotArray[m].myCounter++;
+  }
+  canDraw.globalAlpha = 1;
+  if (moonY > maxY || moonX > maxX + 40 || moonX < -40 && fadeOut < 1) {
+    canDraw.fillStyle = 'rgba(0,0,0,' + fadeOut + ')';
+    canDraw.fillRect(0,0,maxX,maxY);
+    fadeOut += 0.001;
+  }
+  if (fadeOut >= 1) {
+    fadeOut = 0;
+    moonY = -50;
+    moonX = 100 + (Math.random() * (innerWidth - 100));
+  }
+  window.requestAnimationFrame(draw);
 }
 
 draw();
